426248300ef797166351449e552bee05b3cb0c00
Zhengyu Qian <laerhsif@gmail.com>
2016-10-23 05:24:08 +0800

    Make the simple drawing program

---
+++ javascript/_app.js
@@ -0,0 +1,4 @@
+/**
+ * Assembly list:
+ * - main.js
+ */
---
+++ javascript/main.js
@@ -0,0 +1,36 @@
+/* global Chance, fetch, Papa */
+
+(function() {
+	'use strict';
+	
+	var DONATORS_CSV_URL = '/csv/donators.csv';
+	var FETCH_GET_INIT = {
+		method: 'GET',
+		mode: 'cors',
+	};
+	var CSV_CONFIG = {
+		header: true,
+	};
+	
+	var typeChance = new Chance(Date.now());
+	
+	fetch(DONATORS_CSV_URL, FETCH_GET_INIT).then(function(res) {
+		if (res.ok) {
+			return res.text();
+		} else {
+			var error = new Error(res.statusText);
+			error.res = res;
+			throw error;
+		}
+	}).then(function(text) {
+		var donators = Papa.parse(text, CSV_CONFIG);
+		console.debug(donators);
+		
+		var luck = typeChance.integer({
+			min: 0,
+			max: donators.data.length - 1,
+		});
+		console.log('The lucky one: ' + donators.data[luck].id);
+	});
+
+})();
