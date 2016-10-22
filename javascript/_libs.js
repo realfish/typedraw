e501e2ff19fc8aad5dc62ca8008c554b6c2f63e4
Zhengyu Qian <laerhsif@gmail.com>
2016-10-23 05:21:51 +0800

    Build scaffolds
    + Jade template.
    + JS dependencies: vendors and polyfills.
    + Less: depending on Normalize.

---
+++ javascript/_libs.js
@@ -0,0 +1,15 @@
+/**
+ * Assemble list:
+ * _vendor/
+ *   chance.js
+ *   papparse.js
+ * _polyfill/
+ *   fetch.js
+ */
+
+/**
+ * @license
+ * Chance.js v1.0.4 | (c) 2013 Victor Quinn | MIT license | chancejs.com
+ * Papa Parse v4.1.2 | (c) 2013–2015 Matt Holt | MIT license | PapaParse.com
+ * Fetch polyfill | Copyright (c) 2014-2016 GitHub | MIT license | github.com/github/fetch
+ */
