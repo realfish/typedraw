cb6d4d40ab0ecc3f6b423111bf617fefae6da61b
Zhengyu Qian <laerhsif@gmail.com>
2016-10-24 07:21:53 +0800

    Add doT.js as a JS dep

--- javascript/_libs.js
+++ javascript/_libs.js
@@ -10,6 +10,7 @@
 /**
  * @license
  * Chance.js v1.0.4 | (c) 2013 Victor Quinn | MIT license | chancejs.com
+ * doT.js v1.0.1 | (c) 2011–2014 Laura Doktorova | MIT license | github.com/olado/doT
  * Papa Parse v4.1.2 | (c) 2013–2015 Matt Holt | MIT license | PapaParse.com
  * Fetch polyfill | Copyright (c) 2014-2016 GitHub | MIT license | github.com/github/fetch
  */
